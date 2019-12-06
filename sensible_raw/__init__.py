41bd683c2cdd111ba302eaf358cc0a0e84fdb2f5
Ulf Aslak <ulfjensen@gmail.com>
2019-12-06 12:56:33 +0100

    Add a short docstring so users can go ?sensible_raw and get started easily.

--- sensible_raw/__init__.py
+++ sensible_raw/__init__.py
@@ -1,1 +1,8 @@
+"""API for loading data from the Sensible DTU database.
+
+Example usage:
+--------------
+>>> from sensible_raw.loaders import loader
+>>> df = loader.load_data("sms", "january_2014")
+"""
 __author__ = 'radugatej'
